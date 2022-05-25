047949a926ac70584150402b05e5a3da7e6ea8cb
caiosuzuki <caiosuzuki@digix.com.br>
2022-05-24 21:24:01 -0400

    migrating to a real database, using typeorm, injecting dependencies using tsyringe

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
 class ListCategoriesController {
-  constructor(private listCategoriesUseCase: ListCategoriesUseCase) {}
-
-  handle(request: Request, response: Response) {
-    return response.json(this.listCategoriesUseCase.execute());
+  async handle(request: Request, response: Response): Promise<Response> {
+    const listCategoriesUseCase = container.resolve(ListCategoriesUseCase);
+    return response.json(await listCategoriesUseCase.execute());
   }
 }
 
