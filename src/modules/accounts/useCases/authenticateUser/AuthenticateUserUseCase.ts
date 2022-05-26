1005ee19c16f67bc1aa65119020f5d32550041df
caiosuzuki <caiosuzuki@digix.com.br>
2022-05-25 22:22:18 -0400

    creating users model, creating endpoint to authenticate user

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,52 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+    if (!user) {
+      throw new Error("Email or password is incorrect!");
+    }
+
+    const passwordsMatch = await compare(password, user.password);
+    if (!passwordsMatch) {
+      throw new Error("Email or password is incorrect!");
+    }
+
+    const token = sign({}, "61bd2163334120abd745e5fd48b31849", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    return {
+      user: {
+        name: user.name,
+        email,
+      },
+      token,
+    };
+  }
+}
+
+export { AuthenticateUserUseCase };
