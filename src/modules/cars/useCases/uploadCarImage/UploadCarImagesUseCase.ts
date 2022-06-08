73333e78a248a50c42b055a0214caf4eba11ec7d
caiosuzuki <caiosuzuki@digix.com.br>
2022-06-08 19:45:56 -0400

    creating endpoint to upload car images

---
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImagesUseCase.ts
@@ -0,0 +1,23 @@
+import { inject, injectable } from "tsyringe";
+
+import { ICarImagesRepository } from "@modules/cars/repositories/ICarImagesRepository";
+
+interface IRequest {
+  car_id: string;
+  images_names: string[];
+}
+
+@injectable()
+class UploadCarImagesUseCase {
+  constructor(
+    @inject("CarImagesRepository")
+    private carImagesRepository: ICarImagesRepository
+  ) {}
+
+  async execute({ car_id, images_names }: IRequest): Promise<void> {
+    images_names.map(async (image) => {
+      await this.carImagesRepository.create(car_id, image);
+    });
+  }
+}
+export { UploadCarImagesUseCase };
--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,17 +1,23 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "@config/upload";
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
 import { ListAvailableCarsController } from "@modules/cars/useCases/listCars/ListAvailableCarsController";
+import { UploadCarImagesController } from "@modules/cars/useCases/uploadCarImage/UploadCarImagesController";
 
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+
+const upload = multer(uploadConfig.upload("./tmp/cars"));
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
 const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarImageController = new UploadCarImagesController();
 
 carsRoutes.post(
   "/",
@@ -25,6 +31,14 @@ carsRoutes.post(
   ensureAuthenticated,
   ensureAdmin,
   createCarSpecificationController.handle
+);
+
+carsRoutes.post(
+  "/images/:id",
+  ensureAuthenticated,
+  ensureAdmin,
+  upload.array("images"),
+  uploadCarImageController.handle
 );
 
 carsRoutes.get("/available", listAvailableCarsController.handle);
