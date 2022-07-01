ff4d44e37b0c7b67932403cbbfb4ce93261b8e02
caiosuzuki <caiosuzuki@digix.com.br>
2022-07-01 17:59:01 -0400

    including cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import "dotenv/config";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
+import cors from "cors";
 
 import "reflect-metadata";
 import upload from "@config/upload";
@@ -22,6 +23,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
